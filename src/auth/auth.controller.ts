cca0f527ecdd03266ca61ed6b5c468cf83345786
unknown <danilocrabi@gmail.com>
2021-05-04 00:57:58 -0300

    Project task management

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+
+@Controller('auth')
+export class AuthController {
+  constructor(
+    private authService: AuthService,
+  ){}
+
+@Post('/signup')
+  signUp(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise<void>{
+    return this.authService.userAuth(authCredentialsDto);  }
+
+@Post('/signin')
+  signIn(@Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto): Promise< {acessToken: string} >{
+    console.log(authCredentialsDto);
+    return this.authService.signIn(authCredentialsDto);  }    
+}
---
+++ src/tasks/dto/create-task.dto.ts
@@ -0,0 +1,9 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateTaskDto {
+  @IsNotEmpty()
+  title: string;
+  @IsNotEmpty()
+  description: string;
+  
+}
